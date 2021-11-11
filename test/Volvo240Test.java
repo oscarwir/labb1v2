37b3fdf118905877372d6b691b563d4baee0cda7
Jacob Westerberg <jacob.s.westerberg@gmail.com>
2021-11-11 14:29:19 +0100

    added foundation for mr Adam

---
+++ test/Volvo240Test.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Volvo240Test {
+
+    Volvo240 testVolvo = new Volvo240();
+
+
+    @Test
+    void gas_should_increase_car_speed() {
+        testVolvo.gas(0.5);
+        assertEquals(0.9375, testVolvo.getCurrentSpeed());
+    }
+
+    @Test
+    void gas_should_not_decrease_car_speed() {
+        double oldSpeed = testVolvo.getCurrentSpeed();
+        testVolvo.gas(0.5);
+        assertTrue(oldSpeed < testVolvo.getCurrentSpeed());
+    }
+
+    @Test
+    void gas_should_not_increase_car_speed_if_given_amount_is_zero() {
+        testVolvo.gas(0);
+        assertEquals(0, testVolvo.getCurrentSpeed());
+    }
+
+    @Test
+    void brake_should_decrease_speed() {
+        testVolvo.gas(0.5);
+        testVolvo.brake(0.5);
+        assertEquals(0, testVolvo.getCurrentSpeed());
+    }
+
+    @Test
+    void brake_should_not_increase_speed() {
+        double oldSpeed = testVolvo.getCurrentSpeed();
+        testVolvo.decrementSpeed(0.5);
+        assertTrue(oldSpeed >= testVolvo.getCurrentSpeed());
+    }
+
+    @Test
+    void brake_should_not_decrease_speed_if_given_amount_is_zero() {
+        testVolvo.decrementSpeed(0);
+        assertEquals(0, testVolvo.getCurrentSpeed());
+    }
+}
