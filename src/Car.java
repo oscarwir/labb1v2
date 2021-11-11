63196ae51cdf1e6528e620fe910a0609967bd648
Jacob Westerberg <jacob.s.westerberg@gmail.com>
2021-11-11 13:45:57 +0100

    gas and break only accept values in the interval between 0 and 1

--- src/Car.java
+++ src/Car.java
@@ -74,14 +74,17 @@ public abstract class Car implements Movable{
 
     // TODO fix this method according to lab pm
     public void gas(double amount) {
-        incrementSpeed(amount);
-
+       if (amount > 0 && amount <= 1){
+            incrementSpeed(amount);
+        }
     }
 
 
     // TODO fix this method according to lab pm
     public void brake(double amount) {
-        decrementSpeed(amount);
+        if (amount > 0 && amount <= 1) {
+            decrementSpeed(amount);
+        }
     }
 
     public void move(){
