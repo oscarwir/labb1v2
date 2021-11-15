4276bf45e1542b93026993658fb0ab259654d046
Jacob Westerberg <jacob.s.westerberg@gmail.com>
2021-11-15 11:13:24 +0100

    Tried integrating a graphical interface

--- src/test.java
+++ src/test.java
@@ -1,36 +1,146 @@
-/*
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.Transparency;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-// Old test file without JUnit
-// This isn't used
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-public class test {
-    public static void main(String[] args){
-        Saab95 min_fina_saab = new Saab95();
-        Volvo240 min_fina_volvo = new Volvo240();
+public class test extends Thread {
+    private boolean isRunning = true;
+    private Canvas canvas;
+    private BufferStrategy strategy;
+    private BufferedImage background;
+    private Graphics2D backgroundGraphics;
+    private Graphics2D graphics;
+    private JFrame frame;
+    private int width = 320;
+    private int height = 240;
+    private int scale = 1;
+    private GraphicsConfiguration config =
+            GraphicsEnvironment.getLocalGraphicsEnvironment()
+                    .getDefaultScreenDevice()
+                    .getDefaultConfiguration();
 
-        test_increment_speed(min_fina_saab);
+// create a hardware accelerated image
 
-    }
-
-    static void test_increment_speed(Saab95 saab){
-        System.out.println(saab.getCurrentSpeed());
-        saab.incrementSpeed(20);
-        System.out.println(saab.getCurrentSpeed());
-        saab.decrementSpeed(20);
-        System.out.println(saab.getCurrentSpeed());
-    }
-
-    static void test_decrement_speed(){
-
-    }
-
-    static void test_turn_left(){
-
-    }
-
-    static void test_turn_right(){
-
-    }
-
+public final BufferedImage create(final int width, final int height, final boolean alpha) {
+        return config.createCompatibleImage(width, height, alpha ? Transparency.TRANSLUCENT : Transparency.OPAQUE);
 }
-*/
+
+// Setup
+public test() {
+        // JFrame
+        frame = new JFrame();
+        frame.addWindowListener(new FrameClose());
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        frame.setSize(width * scale, height * scale);
+        frame.setVisible(true);
+
+        // Canvas
+        canvas = new Canvas(config);
+        canvas.setSize(width * scale, height * scale);
+        frame.add(canvas, 0);
+
+        // Background & Buffer
+        background = create(width, height, false);
+        canvas.createBufferStrategy(2);
+        do {
+        strategy = canvas.getBufferStrategy();
+        } while (strategy == null);
+        start();
+}
+
+private class FrameClose extends WindowAdapter {
+    @Override
+    public void windowClosing(final WindowEvent e) {
+        isRunning = false;
+    }
+}
+
+    // Screen and buffer stuff
+    private Graphics2D getBuffer() {
+        if (graphics == null) {
+            try {
+                graphics = (Graphics2D) strategy.getDrawGraphics();
+            } catch (IllegalStateException e) {
+                return null;
+            }
+        }
+        return graphics;
+    }
+
+    private boolean updateScreen() {
+        graphics.dispose();
+        graphics = null;
+        try {
+            strategy.show();
+            Toolkit.getDefaultToolkit().sync();
+            return (!strategy.contentsLost());
+
+        } catch (NullPointerException | IllegalStateException e) {
+            return true;
+
+        }
+    }
+
+    public void run() {
+        backgroundGraphics = (Graphics2D) background.getGraphics();
+        long fpsWait = (long) (1.0 / 30 * 1000);
+        main: while (isRunning) {
+            long renderStart = System.nanoTime();
+            updateGame();
+
+            // Update Graphics
+            do {
+                Graphics2D bg = getBuffer();
+                if (!isRunning) {
+                    break main;
+                }
+                renderGame(backgroundGraphics); // this calls your draw method
+                // thingy
+                if (scale != 1) {
+                    assert bg != null;
+                    bg.drawImage(background, 0, 0, width * scale, height
+                            * scale, 0, 0, width, height, null);
+                } else {
+                    assert bg != null;
+                    bg.drawImage(background, 0, 0, null);
+                }
+                bg.dispose();
+            } while (!updateScreen());
+
+            // Better do some FPS limiting here
+            long renderTime = (System.nanoTime() - renderStart) / 1000000;
+            try {
+                Thread.sleep(Math.max(0, fpsWait - renderTime));
+            } catch (InterruptedException e) {
+                Thread.interrupted();
+                break;
+            }
+            renderTime = (System.nanoTime() - renderStart) / 1000000;
+
+        }
+        frame.dispose();
+    }
+
+    public void updateGame() {
+        // update game logic here
+    }
+
+    public void renderGame(Graphics2D g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, width, height);
+    }
+
+    public static void main(final String[] args) {
+        new test();
+    }
+}
