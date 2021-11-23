d9002ce0e152c6b1ff3ab43d8ac60682da45e393
oscar <skofta@live.se>
2021-11-23 11:04:53 +0100

    stuff

--- src/CarHaulerX2000CargoPlatform.java
+++ src/CarHaulerX2000CargoPlatform.java
@@ -85,6 +85,7 @@ public class CarHaulerX2000CargoPlatform implements CargoPlatform{
                 loadCargo(car);
                 moveCargo();
                 turnCargo();
+                currentCarAmount++;
             }
         }
     }
