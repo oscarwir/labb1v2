b86082dded8bb43b820b57ead068f9dbd4f61473
Phozel <adamwi@student.chalmers.se>
2021-11-24 15:33:21 +0100

    Added JavaDocs comments to CarHaulerX2000

--- src/CarHaulerX2000.java
+++ src/CarHaulerX2000.java
@@ -6,11 +6,17 @@ public class CarHaulerX2000 extends Truck{
         super("CarHaulerX2000", 5, new PointDouble(200,100), new Direction(0), 2, Color.black,400, 3);
     }
 
+    /**
+     * @return a cargoPlatform
+     */
     @Override
     protected CargoPlatform setCargoPlatform(){
         return new CarHaulerX2000CargoPlatform(this);
     }
 
+    /**
+     * @return speedFactor based on enginePower
+     */
     @Override
     protected double speedFactor() {
         return getEnginePower() * 0.01;
