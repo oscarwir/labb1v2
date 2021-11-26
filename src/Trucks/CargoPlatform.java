a65e3ad28179be1429cc9e286357e1ce97977eb6
oscarwir <skofta@live.se>
2021-11-26 09:58:16 +0100

    awdawd

--- src/Trucks/CargoPlatform.java
+++ src/Trucks/CargoPlatform.java
@@ -1,48 +1,30 @@
 package Trucks;
 import Head.Movable;
 
-/**
- *
- */
+
 public interface CargoPlatform {
 
-    /**
-     *
-     */
+
     void moveCargo();
 
-    /**
-     *
-     */
+
     void turnCargo();
 
-    /**
-     *
-     */
+
     void unloadCargo();
 
-    /**
-     * @param object
-     */
+
     void loadCargo(Movable object);
 
-    /**
-     *
-     */
+
     void raisePlatform();
 
-    /**
-     *
-     */
+
     void lowerPlatform();
 
-    /**
-     * @return
-     */
+
     boolean isPlatformFullyOpen();
 
-    /**
-     * @return
-     */
+
     boolean isPlatformClosed();
 }
