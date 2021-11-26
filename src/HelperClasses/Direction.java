8b12f51279b1713d123c2637b22db0db071fa1b0
oscarwir <skofta@live.se>
2021-11-26 10:07:26 +0100

    awdawd

--- src/HelperClasses/Direction.java
+++ src/HelperClasses/Direction.java
@@ -9,21 +9,22 @@ public class Direction {
     protected int angle;
 
     /**
-     * @param angle
+     * Constructor for Direction.
+     * @param angle in degrees.
      */
     public Direction(int angle){
         this.angle = angle % 360; //We use modulus to exclude potential overlap of degrees. We only want degrees in the interval between 0 and 360.
     }
 
     /**
-     * @param angle
+     * @param angle in degrees.
      */
     public void setDirection(int angle){
         this.angle = angle % 360;
     }
 
     /**
-     * @return
+     * @return current angle of direction.
      */
     public int getDirection(){
         return angle;
