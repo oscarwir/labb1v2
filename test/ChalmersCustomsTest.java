5106bbb4ec49295dd4c49d385c9c8128a6022f9a
Jacob Westerberg <jacob.s.westerberg@gmail.com>
2021-11-26 10:47:33 +0100

    bonk

--- test/ChalmersCustomsTest.java
+++ test/ChalmersCustomsTest.java
@@ -1,2 +1,17 @@
+import Cars.Car;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
 public class ChalmersCustomsTest {
+
+    @Test
+    void load_car_should_load_volvo(){
+
+    }
+
+    @Test
+    void load_car_should_not_load_saab(){
+
+    }
+
 }
