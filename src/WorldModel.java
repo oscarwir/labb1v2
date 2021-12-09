4d40151dfa95f261fe87176728d3ff11f5c64eee
Jacob Westerberg <jacob.s.westerberg@gmail.com>
2021-12-09 15:06:20 +0100

    bonk

--- src/WorldModel.java
+++ src/WorldModel.java
@@ -1,2 +1,14 @@
-public class WorldModel {
+import java.util.ArrayList;
+import java.util.Timer;
+
+public class WorldModel implements IObserver{
+
+    private int width;
+    private int height;
+    private ArrayList <IObserver> observers;
+    private int delay;
+    private Timer timer;
+
+
+
 }
